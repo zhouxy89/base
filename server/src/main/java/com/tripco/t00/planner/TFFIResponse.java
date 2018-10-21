7fe6e5845116e07a5f46f1738fb7292779339b6a
Livingwind <westerman.christopher@gmail.com>
2018-10-20 21:13:17 -0600

    Constructed a base class for Config and Distance named TFFIResponse which could later be used for a factory pattern to simplify the MicroServer.
    closes #10

---
+++ server/src/main/java/com/tripco/t00/planner/TFFIResponse.java
@@ -0,0 +1,6 @@
+package com.tripco.t00.planner;
+
+public abstract class TFFIResponse {
+  protected short version;
+  protected String type;
+}
--- server/src/main/java/com/tripco/t00/server/Config.java
+++ server/src/main/java/com/tripco/t00/server/Config.java
@@ -1,18 +1,19 @@
 package com.tripco.t00.server;
 
 import com.google.gson.Gson;
+import com.tripco.t00.planner.TFFIResponse;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class Config {
-
-  private short version = 1;
-  private String type = "config";
-
+public class Config extends TFFIResponse {
   private List<String> units = Arrays.asList("miles", "kilometers", "nautical miles");
 
-  static String getConfig() {
+  private Config() {
+    type = "config";
+  }
+
+  public static String buildResponse() {
     Config conf = new Config();
     Gson gson = new Gson();
 
