a6fbec73098b5d0a6fe0799885da44131352a4a2
Dave Matthews <davemattcsu@gmail.com>
2018-12-17 15:01:06 -0700

    revised calculator layout to separate layout from contents/computations.

--- client/src/components/Application/Calculator/Calculator.js
+++ client/src/components/Application/Calculator/Calculator.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { Container, Row, Col } from 'reactstrap'
-import { Card, CardBody } from 'reactstrap'
+import { Card, CardHeader, CardBody } from 'reactstrap'
 import { Button } from 'reactstrap'
 import { Form, Label, Input } from 'reactstrap'
 import { request } from '../../../api/api.js'
 
 export default class Calculator
-  extends Component {
+    extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -43,50 +43,65 @@ export default class Calculator
     };
 
     request(body, 'distance', this.props.options.hostname).then(
-      (response) => {
-        this.setState({distance: response.distance});
-      }
+        (response) => {
+          this.setState({distance: response.distance});
+        }
     );
   }
 
   create_input_fields(title, id) {
     let hoc_update = (e) => {this.update_input(id, e.target.name, e.target.value)};
     return (
-      <Form inline>
-              <Col md={4} lg={2}>
-                <Label>{title}</Label>
-              </Col>
-              <Col sm={6} md={4} lg={4}>
-                <Input name='latitude' onChange={hoc_update} value={this.state[id]['latitude']}
-                       style={{width: "100%"}} placeholder="Latitude"/>
-              </Col>
-              <Col sm={6} md={4} lg={4}>
-                <Input name='longitude' onChange={hoc_update} value={this.state[id]['longitude']}
-                       style={{width: "100%"}} placeholder="Longitude"/>
-              </Col>
-      </Form>
-      );
+        <Card>
+          <CardHeader>{title}</CardHeader>
+          <CardBody>
+            <Form >
+              <Input name='latitude' onChange={hoc_update} value={this.state[id]['latitude']}
+                     style={{width: "100%"}} placeholder="Latitude"/>
+              <Input name='longitude' onChange={hoc_update} value={this.state[id]['longitude']}
+                     style={{width: "100%"}} placeholder="Longitude"/>
+            </Form>
+          </CardBody>
+        </Card>
+    );
+  }
+
+  create_distance() {
+    return(
+        <Card>
+          <CardHeader>is</CardHeader>
+          <CardBody>
+            <h5>{this.state.distance} {this.props.options.unit}</h5>
+            <Button onClick={this.calculate_distance}>Calculate</Button>
+          </CardBody>
+        </Card>
+    )
   }
 
   render() {
     return (
-      <Container>
-          {this.create_input_fields('Origin', 'first')}
-          {this.create_input_fields('Destination', 'second')}
-
-          <Card>
-            <CardBody>
-              <Row>
-                <Col md={6}>
-                  <Button onClick={this.calculate_distance}>Calculate</Button>
-                </Col>
-                <Col md={6}>
-                  {this.state.distance} {this.props.options.unit}
-                </Col>
-              </Row>
-            </CardBody>
-          </Card>
-      </Container>
-    )
+        <Container>
+          <Row>
+            <Col>
+              <Card>
+                <CardHeader>
+                Calculator:  The distance...
+                </CardHeader>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              {this.create_input_fields('from', 'first')}
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              {this.create_input_fields('to', 'second')}
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              {this.create_distance()}
+            </Col>
+          </Row>
+        </Container>
+    );
   }
 }
