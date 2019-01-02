3ead2959fca6d54b97b05c23eb0caf279967803f
Caleb Carlson <ccarlson355@gmail.com>
2019-01-01 18:33:06 -0700

    Added basic skeleton to write tests for Application.js

---
+++ client/test/Application.test.js
@@ -0,0 +1,29 @@
+import './enzyme.config.js'                   // (1)
+import React from 'react'
+import { shallow } from 'enzyme'              // (2)
+import Application from '../src/components/Application/Application'
+
+function testConstructor() {
+    const app = shallow(<Application/>);
+
+    let actualConfig = app.state().config;
+    let expectedConfig = null;
+    expect(actualConfig).toEqual(expectedConfig);
+
+    let actualOptions = app.state().options;
+    let expectedOptions = {
+        units: { miles: 3959, kilometers: 6371 },
+        unit: 'miles',
+        serverPort: 'localhost:'
+    };
+
+    expect(actualConfig).toEqual(expectedConfig);
+}
+
+test("Testing Application's constructor", testConstructor);
+
+function testTrue() {
+    expect(true).toEqual(true);
+}
+
+test("Testing basic boolean equivalency", testTrue);
