8d5e9ffadc170af81fd0b7b4df7e7e0756406e56
Dave Matthews <davemattcsu@gmail.com>
2019-01-11 14:55:56 -0700

    clean code. pull navigation out of header into app.

--- client/src/components/Marginals/Header.js
+++ client/src/components/Marginals/Header.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './css/marginals.css';
-import Navigation from "./Navigation";
 import {HeaderLogoLarge, HeaderLogoMedium, HeaderLogoSmall} from './HeaderLogo';
 
 /* Renders a text heading above the application with useful information.
@@ -12,34 +11,24 @@ export default class Header extends Component{
 
   render() {
     return(
-      <div>
-        {this.topLevelHeader()}
-        <Navigation pages={this.props.pages} setAppPage={this.props.setAppPage}/>
-        <div className="add-title"/> {/* Background box to header */}
-      </div>
-    );
-  }
-
-  topLevelHeader() {
-    return (
-      <div className="add-header-height">
-        <div className="application-width">
-          <div id="responsiveHeaderContainer">
-            <a href="http://colostate.edu" id="csuHeaderLink" target="_blank">
-              <HeaderLogoLarge/>
-              <HeaderLogoMedium/>
-              <HeaderLogoSmall/>
-            </a>
-            <div id="responsiveLogoSubsystem">
-              <a href={"https://compsci.colostate.edu/"} id="csHeaderLink" target="_blank">
-                <h1 className="larger-CSUtext-upper">
-                  Computer Science
-                </h1>
+        <div className="add-header-height">
+          <div className="application-width">
+            <div id="responsiveHeaderContainer">
+              <a href="http://colostate.edu" id="csuHeaderLink" target="_blank">
+                <HeaderLogoLarge/>
+                <HeaderLogoMedium/>
+                <HeaderLogoSmall/>
               </a>
+              <div id="responsiveLogoSubsystem">
+                <a href={"https://compsci.colostate.edu/"} id="csHeaderLink" target="_blank">
+                  <h1 className="larger-CSUtext-upper">
+                    Computer Science
+                  </h1>
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
     );
   }
 
