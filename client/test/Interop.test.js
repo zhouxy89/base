537b8d1e8664394035ee9335f78c1a5d0972074d
Caleb Carlson <ccarlson355@gmail.com>
2019-01-17 16:23:48 -0700

    Created tests for Interop.js

--- client/test/Interop.test.js
+++ client/test/Interop.test.js
@@ -1,13 +1,71 @@
 import './enzyme.config.js';
 import React from 'react';
+import {Input} from 'reactstrap';
 import {mount, shallow} from 'enzyme';
-import {sendServerRequest} from '../src/api/restfulAPI';
 import Application from '../src/components/Application/Application';
 import Interop from '../src/components/Application/Settings/Interop';
-import Settings from '../src/components/Application/Settings/Settings';
 
+const startProperties = {
+  'serverPort': 'black-bottle.cs.colostate.edu:31400',
+  'updateSetting': () => {
+  }
+};
 
-function testApiCall() {
+function testInputExists() {
+  const interop = shallow(<Interop serverPort={startProperties.serverPort}
+                                   updateSetting={startProperties.updateSetting}/>);
+
+  expect(interop.contains(<Input onChange={interop.instance().updateInputText}
+                                 value={interop.state().inputText}
+                                 placeholder={startProperties.serverPort}/>)).toEqual(true);
+}
+
+test('Testing that an Input is rendered', testInputExists);
+
+function testupdateInputText() {
+  const interop = mount(<Interop serverPort={startProperties.serverPort}
+                                 updateSetting={startProperties.updateSetting}/>);
+
+  expect(interop.state().inputText).toEqual('');
+
+  let inputText = 'Fake input text';
+  simulateOnChangeEvent(inputText, interop);
+  expect(interop.state().inputText).toEqual(inputText);
+}
+
+function simulateOnChangeEvent(inputText, reactWrapper) {
+  let event = {target: {value: inputText}};
+  reactWrapper.find('Input').simulate('change', event);
+  reactWrapper.update();
+}
+
+test('Testing that the onChange event for Input updates the inputText field in'
+    + 'state', testupdateInputText);
+
+function testUpdateServerPort() {
+  mockConfigResponse();
+
+  const application = mount(<Application/>);
+
+  let actualBeforeServerPort = application.state().clientSettings.serverPort;
+  let expectedBeforeServerPort = location.hostname + ':';
+  expect(actualBeforeServerPort).toEqual(expectedBeforeServerPort);
+
+  const interop = application.find('Interop');
+
+  let inputText = 'black-bottle.cs.colostate.edu:31400';
+  simulateOnChangeEvent(inputText, interop);
+  interop.find('form').simulate('submit', { target: interop.find('Input') } );
+
+  let actualAfterServerPort = application.state().clientSettings.serverPort;
+  let expectedAfterServerPort = inputText;
+  expect(actualAfterServerPort).toEqual(expectedAfterServerPort);
+}
+
+test('Testing that the onClick event for the form updates the serverPort field '
+    + 'in Application', testUpdateServerPort);
+
+function mockConfigResponse() {
   fetch.mockResponse(JSON.stringify(
       {
         status: 200,
@@ -16,23 +74,11 @@ function testApiCall() {
           'placeAttributes': ["latitude", "longitude", "serverName"],
           'requestType': "config",
           'requestVersion': 1,
-          'serverName': "t00"},
+          'serverName': "t00"
+        },
         type: 'basic',
         url: 'http://localhost:8088/api/config',
         redirected: false,
-        ok: true}));
-
-  /*
-  expect.assertions(1);
-  return sendServerRequest('black-bottle.cs.colostate.edu:31400', 'config')
-  .then(response => {
-    expect(response).toEqual({'body': { 'body': 1234, 'status': 200, 'statusText': 'OK'},'statusCode': 200, 'statusText': 'OK'});
-  });
-  */
-
-  const app = mount(<Application/>);
-
+        ok: true
+      }));
 }
-
-
-test('testing api call', testApiCall);
