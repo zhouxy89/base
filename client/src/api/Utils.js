eddc2bdf0562bd0b5ab003ca57cf5b18a8d859fe
Caleb <ccarlson355@gmail.com>
2019-12-09 13:36:34 -0700

    Export validation function, add ajv dependency

---
+++ client/src/api/Utils.js
@@ -0,0 +1,9 @@
+/* General helpful functions which are used App-wide */
+
+export function isValid(object, schema) {
+  let Ajv = require('ajv');
+  let ajv = new Ajv();
+  let validate = ajv.compile(schema);
+  return validate(object);
+}
+
