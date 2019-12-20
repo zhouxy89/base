e0dfca84da13c7e64db134c1b0272c2f7836c757
Caleb Carlson <cacaleb@rams.colostate.edu>
2019-12-20 16:45:59 -0700

    Simplify schema loading

--- server/src/main/java/com/tripco/t00/misc/JSONValidator.java
+++ server/src/main/java/com/tripco/t00/misc/JSONValidator.java
@@ -4,12 +4,10 @@ import com.tripco.t00.TIP.TIPConfig;
 import com.tripco.t00.TIP.TIPDistance;
 import com.tripco.t00.TIP.TIPHeader;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InvalidClassException;
 import java.lang.reflect.Type;
-import java.nio.file.Path;
 import org.everit.json.schema.SchemaException;
 import org.everit.json.schema.Schema;
 import org.everit.json.schema.loader.SchemaLoader;
@@ -27,22 +25,35 @@ import org.slf4j.LoggerFactory;
  */
 public class JSONValidator {
 
+  private final Logger log = LoggerFactory.getLogger(JSONValidator.class);
+
   private Schema schema;
 
-  public JSONValidator(Type requestType) throws InvalidClassException {
+  public JSONValidator(Type requestType) {
     this.schema = getSchemaFromType(requestType);
   }
 
-  private Schema getSchemaFromType(Type requestType) throws InvalidClassException {
+  private Schema getSchemaFromType(Type requestType) {
     String resourcePath = "";
 
     switch (requestType.getTypeName()) {
-      case "TIPDistance": resourcePath = "/TIPDistanceRequestSchema.json"; break;
-      default: throw new InvalidClassException(String.format("Schema resource not found for %s!", requestType.getTypeName()));
+      case "TIPDistance":
+        resourcePath = "/TIPDistanceRequestSchema.json"; break;
+      default:
+        log.info("Unknown class {} specified for schema validation", requestType.getTypeName());
+        return null;
     }
 
+    Schema loadedSchema = null;
+    try (InputStream inputStream = getClass().getResourceAsStream(resourcePath)) {
+      JSONObject rawSchema = new JSONObject(new JSONTokener(inputStream));
+      loadedSchema = SchemaLoader.load(rawSchema);
+    } catch (SchemaException | ValidationException | IOException e) {
+      log.error("Unable to create schema. Root message: {}", e.getMessage());
+      e.printStackTrace();
+    }
 
-    return null;
+    return loadedSchema;
   }
 
 }
