f50b321ef9678ea9046d28548fba6da15b1c3ef4
mrosoff <mrosoff@ramscolostate.edu>
2020-01-24 13:24:47 -0700

    Fix Modal Tests

--- client/src/components/Margins/ServerSettingsModal.js
+++ client/src/components/Margins/ServerSettingsModal.js
@@ -14,43 +14,41 @@ export default class ServerSettingsModal extends Component {
             inputText: this.props.clientSettings.serverPort,
             validServer: true,
             validSave: false,
-            config: false
+            config: {}
         }
     }
 
     render() {
         let currentServerName = this.props.serverConfig && this.state.validServer ? this.props.serverConfig.serverName : '';
-        if (this.state.config) {
+        if (this.state.config && Object.keys(this.state.config).length > 0) {
             currentServerName = this.state.config.body.serverName;
         }
         return (
-            <div>
-                <Modal isOpen={this.props.modalOpen} toggle={() => this.props.toggleModal()}>
-                    <ModalHeader toggle={() => this.props.toggleModal()}>Server Connection</ModalHeader>
-                    <ModalBody>
-                        <p>Name: {currentServerName}</p>
-                        <p style={{float: "left"}}>URL:</p>
-                        <div style={{overflow: "hidden", "paddingLeft": ".5em"}}>
-                            <Input onChange={(e) => this.updateInput(e.target.value)}
-                                   value={this.state.inputText}
-                                   valid={this.state.validServer}
-                                   invalid={!this.state.validServer}
-                            />
-                        </div>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="secondary" onClick={() => this.resetModalState()}>Cancel</Button>
-                        <Button onClick={() => {
-                            this.props.updateServerConfig(this.state.inputText, this.state.config);
-                            this.resetModalState();
-                        }}
-                                disabled={!this.state.validSave}
-                        >
-                            Save
-                        </Button>
-                    </ModalFooter>
-                </Modal>
-            </div>
+            <Modal isOpen={this.props.modalOpen} toggle={() => this.props.toggleModal()}>
+                <ModalHeader toggle={() => this.props.toggleModal()}>Server Connection</ModalHeader>
+                <ModalBody>
+                    <p>Name: {currentServerName}</p>
+                    <p style={{float: "left"}}>URL:</p>
+                    <div style={{overflow: "hidden", "paddingLeft": ".5em"}}>
+                        <Input onChange={(e) => this.updateInput(e.target.value)}
+                               value={this.state.inputText}
+                               valid={this.state.validServer}
+                               invalid={!this.state.validServer}
+                        />
+                    </div>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="secondary" onClick={() => this.resetModalState()}>Cancel</Button>
+                    <Button onClick={() => {
+                        this.props.updateServerConfig(this.state.inputText, this.state.config);
+                        this.resetModalState();
+                    }}
+                            disabled={!this.state.validSave}
+                    >
+                        Save
+                    </Button>
+                </ModalFooter>
+            </Modal>
         )
     }
 
@@ -60,7 +58,7 @@ export default class ServerSettingsModal extends Component {
             inputText: this.props.clientSettings.serverPort,
             validServer: true,
             validSave: false,
-            config: false
+            config: {}
         });
     }
 
@@ -71,7 +69,7 @@ export default class ServerSettingsModal extends Component {
                         this.processConfigResponse(config);
                     });
                 } else {
-                    this.setState({validServer: false, validSave: false, config: false});
+                    this.setState({validServer: false, validSave: false, config: {}});
                 }
             }
         );
@@ -79,7 +77,7 @@ export default class ServerSettingsModal extends Component {
 
     processConfigResponse(config) {
         if (!isValid(config.body, configSchema) || !(config.statusCode >= 200 && config.statusCode <= 299)) {
-            this.setState({validServer: false, validSave: false, config: false});
+            this.setState({validServer: false, validSave: false, config: {}});
         } else {
             this.setState({validServer: true, validSave: true, config: config})
         }
--- client/test/ServerSettingsModal.test.js
+++ client/test/ServerSettingsModal.test.js
@@ -6,8 +6,6 @@ import App from "../src/components/App"
 import Footer from '../src/components/Margins/Footer'
 import ServerSettingsModal from '../src/components/Margins/ServerSettingsModal'
 
-import {Button, Input} from 'reactstrap'
-
 const startProperties = {
     modalOpen: true,
     serverConfig: {},
@@ -22,10 +20,10 @@ function testRender() {
         <Footer
             serverConfig={startProperties.serverConfig}
             clientSettings={startProperties.clientSettings.serverPort}
-            updateServerConfig={(value, config) => this.updateServerConfig(value, config)}
+            updateServerConfig={startProperties.updateServerConfig}
         />);
 
-    expect(footer.find(<ServerSettingsModal/>).length).toEqual(1);
+    expect(footer.find('ServerSettingsModal').length).toEqual(1);
 }
 
 test("Modal component should be rendered inside Footer", testRender);
@@ -42,7 +40,7 @@ function testRenderInput() {
             updateServerConfig={startProperties.updateServerConfig}
         />);
 
-    expect(modal.find(<Input/>).length).toEqual(1);
+    expect(modal.find('Input').length).toEqual(1);
 }
 
 test('An Input field should be rendered inside the Modal', testRenderInput);
@@ -67,7 +65,7 @@ function testUpdateInputText() {
 
 function simulateOnChangeEvent(inputText, reactWrapper) {
     let event = {target: {value: inputText}};
-    reactWrapper.find(<Input/>).at(0).simulate('change', event);
+    reactWrapper.find('Input').at(0).simulate('change', event);
     reactWrapper.update();
 }
 
@@ -76,37 +74,36 @@ test("onChangeEvent should update the Modal's state", testUpdateInputText);
 function testUpdateServerPort() {
     mockConfigResponse();
 
-    const app = mount(<App/>);
+    const app = mount(<App />);
+    const modal = shallow(
+        <ServerSettingsModal
+            modalOpen={startProperties.modalOpen}
+            serverConfig={startProperties.serverConfig}
+            clientSettings={startProperties.clientSettings}
+            toggleModal={startProperties.toggleModal}
+            updateServerConfig={(value, config) => app.instance().updateServerConfig(value, config)}
+        />);
 
     let actualBeforeServerPort = app.state().clientSettings.serverPort;
     let expectedBeforeServerPort = `http://${location.hostname}:`;
-    expect(actualBeforeServerPort).toEqual(expectedBeforeServerPort);
-
-    const modal = app.find(<ServerSettingsModal/>);
 
     let inputText = 'https://black-bottle.cs.colostate.edu:31400';
     simulateOnChangeEvent(inputText, modal);
-    interop.find(<Button/>)[1].simulate('onClick', null);
+    modal.find('Button').at(1).simulate('click');
 
     let actualAfterServerPort = app.state().clientSettings.serverPort;
+
+    expect(actualBeforeServerPort).toEqual(expectedBeforeServerPort);
     expect(actualAfterServerPort).toEqual(inputText);
 }
 
 function mockConfigResponse() {
     fetch.mockResponse(JSON.stringify(
         {
-            status: 200,
-            statusText: 'OK',
-            body: {
-                'placeAttributes': ["latitude", "longitude", "serverName"],
-                'requestType': "config",
-                'requestVersion': 1,
-                'serverName': "t00"
-            },
-            type: 'basic',
-            url: 'http://localhost:8088/api/config',
-            redirected: false,
-            ok: true
+            'placeAttributes': ["latitude", "longitude", "name"],
+            'requestType': "config",
+            'requestVersion': 1,
+            'serverName': "t00"
         }));
 }
 
