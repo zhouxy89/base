3559dcfb82ade8a8de50b271e060cccc499e92bd
mrosoff <mrosoff@ramscolostate.edu>
2020-01-24 15:09:21 -0700

    Final Pass.

--- client/test/Application.test.js
+++ client/test/Application.test.js
@@ -1,17 +1,18 @@
 import './enzyme.config.js';
 import React from 'react';
 import {shallow} from 'enzyme';
-import Application from '../src/components/Application/Application';
 
+import Application from '../src/components/Application/Application';
 
 function testInitialAppState() {
 
   const app = shallow(<Application/>);
 
-  let actualMarkerPosition = app.state().markerPosition,
-      actualMapCenter = app.state().mapCenter,
-      expectedMarkerPosition = null,
-      expectedMapCenter = [0,0,];
+  let actualMarkerPosition = app.state().markerPosition;
+  let actualMapCenter = app.state().mapCenter;
+
+  let expectedMarkerPosition = null;
+  let expectedMapCenter = [0, 0];
 
   expect(actualMarkerPosition).toEqual(expectedMarkerPosition);
   expect(actualMapCenter).toEqual(expectedMapCenter);
