8b2b951cdbfe88ea389788fdc9fab9687d4718cf
mrosoff <mrosoff@ramscolostate.edu>
2020-01-25 16:48:38 -0700

    MicroServer rewrite.

--- server/src/main/java/com/tco/server/MicroServer.java
+++ server/src/main/java/com/tco/server/MicroServer.java
@@ -20,9 +20,6 @@ import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 
 
-/** A micro server for a single page web application that serves the static files
- * and processes restful API requests.
- */
 class MicroServer {
 
   private final Logger log = LoggerFactory.getLogger(MicroServer.class);
@@ -50,7 +47,6 @@ class MicroServer {
     else {
       log.info("MicroServer using HTTP.");
     }
-    log.trace("Server configuration complete");
   }
 
 
@@ -58,26 +54,22 @@ class MicroServer {
     String path = "/public/";
     Spark.staticFileLocation(path);
     Spark.get("/", (req, res) -> { res.redirect("index.html"); return null; });
-    log.trace("Static file configuration complete");
   }
 
 
   private void processRestfulAPIrequests() {
-    Spark.get("/api/config", this::processTIPconfigRequest);
+    Spark.get("/api/config", this::processConfigRequest);
   }
 
 
-  private String processTIPconfigRequest(Request request, Response response) {
-    logRequest(request);
-    response.type("application/json");
-    response.header("Access-Control-Allow-Origin", "*");
-    response.status(200);
+  private String processConfigRequest(Request request, Response response) {
+    setupResponse(request, response);
     try {
       Gson jsonConverter = new Gson();
-      RequestConfig tipRequest = new RequestConfig();
-      tipRequest.buildResponse();
-      String responseBody = jsonConverter.toJson(tipRequest);
-      log.trace("TIP Config response: {}", responseBody);
+      RequestConfig config = new RequestConfig();
+      config.buildResponse();
+      String responseBody = jsonConverter.toJson(config);
+      log.trace("Config response: {}", responseBody);
       return responseBody;
     } catch (Exception e) {
       log.error("Exception: {}", e);
@@ -87,22 +79,19 @@ class MicroServer {
   }
 
 
-  private String processTIPrequest(Type tipType, Request request, Response response) {
-    logRequest(request);
-    response.type("application/json");
-    response.header("Access-Control-Allow-Origin", "*");
-    response.status(200);
-
+  private String processDataRequest(Type type, Request request, Response response) {
+    setupResponse(request, response);
     try {
       Gson jsonConverter = new Gson();
-      RequestHeader tipInstance = createTIPInstance(tipType, request);
-      tipInstance.buildResponse();
-      String responseBody = jsonConverter.toJson(tipInstance);
-      log.trace("TIP Response: {}", responseBody);
+      JSONValidator.validate(type, request.body());
+      RequestHeader data = jsonConverter.fromJson(request.body(), type);
+      data.buildResponse();
+      String responseBody = jsonConverter.toJson(data);
+      log.trace("Data Response: {}", responseBody);
       return responseBody;
 
     } catch (IOException e) {
-      log.error("TIP request failed validation: {}", request.body());
+      log.error("Data request failed validation: {}", request.body());
       log.error("Reason for failure: {}", e.getMessage());
       response.status(400);
       return null;
@@ -113,9 +102,15 @@ class MicroServer {
     }
   }
 
+  private void setupResponse(Request request, Response response) {
+    logRequest(request);
+    response.type("application/json");
+    response.header("Access-Control-Allow-Origin", "*");
+    response.status(200);
+  }
 
   private void logRequest(Request request) {
-    String message = "TIP Request - "
+    String message = "Request - "
             + "[" + dateTimeFormat.format(LocalDateTime.now()) + "] "
             + request.ip() + " "
             + "\"" + request.requestMethod() + " "
@@ -124,13 +119,4 @@ class MicroServer {
             + "[" + request.body() + "]";
     log.error(message);
   }
-
-
-  // Throws an IOException if something went wrong with loading the schema or validating the request.
-  private RequestHeader createTIPInstance(Type classType, Request request) throws IOException {
-    JSONValidator.validate(classType, request.body()); // Validates request against JSON schema
-    Gson jsonConverter = new Gson();
-    return jsonConverter.fromJson(request.body(), classType);
-  }
-
 }
