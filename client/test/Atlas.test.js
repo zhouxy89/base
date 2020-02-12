d19c760cf16f6fe5601d1ec573f36bd2bdf01cf1
Louis Narmour <lnarmour@rams.colostate.edu>
2020-02-11 17:37:18 -0700

    Fix Atlas.test.js remove map center check since it's now a constant

--- client/test/Atlas.test.js
+++ client/test/Atlas.test.js
@@ -9,12 +9,9 @@ function testInitialAppState() {
   const app = shallow(<Atlas />);
 
   let actualMarkerPosition = app.state().markerPosition;
-  let actualMapCenter = app.state().mapCenter;
 
   let expectedMarkerPosition = null;
-  let expectedMapCenter = [0, 0];
 
   expect(actualMarkerPosition).toEqual(expectedMarkerPosition);
-  expect(actualMapCenter).toEqual(expectedMapCenter);
 }
 test("Testing Atlas's Initial State", testInitialAppState);
