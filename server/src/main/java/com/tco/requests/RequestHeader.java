c2ba28376b9920bab6c73c05d62b654e4e4bd247
mrosoff <mrosoff@ramscolostate.edu>
2020-02-20 08:07:08 -0700

    JUnit5 and Server Updates

---
+++ server/src/main/java/com/tco/requests/RequestHeader.java
@@ -0,0 +1,19 @@
+package com.tco.requests;
+
+public abstract class RequestHeader {
+
+  protected final static int CURRENT_SUPPORTED_VERSION = 1;
+
+  protected String requestType;
+  protected Integer requestVersion;
+
+  public String getRequestType() {
+    return requestType;
+  }
+
+  public Integer getRequestVersion() {
+    return requestVersion;
+  }
+
+  public abstract void buildResponse();
+}
